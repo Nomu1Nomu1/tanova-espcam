c55ce5f300c649a45107cbce91bbe2e95b6fb0b0
HRF-10 <hanifraditya09@gmail.com>
2026-09-04 13:01:11 +0700

    update buzzer

--- src/main.cpp
+++ src/main.cpp
@@ -307,6 +307,10 @@ void setup()
     Serial.setDebugOutput(true);
     Serial.println();
 
+    // --- FIX: inisialisasi buzzer sebagai OUTPUT ---
+    pinMode(BUZZER_PIN, OUTPUT);
+    digitalWrite(BUZZER_PIN, LOW);
+
     camera_config_t config;
     config.ledc_channel = LEDC_CHANNEL_0;
     config.ledc_timer = LEDC_TIMER_0;
