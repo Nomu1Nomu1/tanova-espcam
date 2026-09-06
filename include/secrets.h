abcaa6060cc4c15be96e717fd0240ba186d51e5f
Nomu1Nomu1 <isaiah.tino@gmail.com>
2026-09-06 12:18:31 +0700

    mine

--- include/secrets.h
+++ include/secrets.h
@@ -1,10 +1,12 @@
 #pragma once
 
 // WiFi Credentials
-#define WIFI_SSID "54545"
-#define WIFI_PASSWORD "1234567890"
+// #define WIFI_SSID "54545"
+#define WIFI_SSID "Maximizer_2.4G"
+// #define WIFI_PASSWORD "1234567890"
+#define WIFI_PASSWORD "WhyYouAskForIt"
 
 // Roboflow Configuration
-#define ROBOFLOW_API_KEY ""
-#define ROBOFLOW_MODEL ""
+#define ROBOFLOW_API_KEY "rAEP2opSNkG18AwsWkQq"
+#define ROBOFLOW_MODEL "plant-disease-detection-k6wnw"
 
